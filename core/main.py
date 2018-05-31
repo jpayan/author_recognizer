423d2049650e0dabfee72562ca835ae70cd2c2e3
Payan, Jorge <jorge.payan@thermofisher.com>
2018-05-30 17:07:53 -0700

    Add random phrase selection for testing

--- core/main.py
+++ core/main.py
@@ -1,21 +1,32 @@
 import os
+import random
 
 from train_model import ModelTrainer
 from test_model import ModelTester
 from utils.io_utils import get_dir_files
 
 
+def generate_testing_file(training_file_path, test_file_path):
+    with open(training_file_path, "r", encoding="utf8") as training_file:
+        lines = training_file.readlines()
+        phrase = random.choice(lines)
+
+    with open(test_file_path, "w", encoding="utf8") as test_file:
+        test_file.write(phrase)
+
+
 def generate_training_data_from_dataset(dataset_path):
     files = get_dir_files(dataset_path)
     data = ""
     for file in files:
-        if file == ".DS_Store":
-            continue
-        with open(os.path.join(dataset_path, file), "r", encoding="utf8") as stream:
-            data += stream.read()
-    training_file_path = "{}.txt".format(os.path.basename(dataset_path))
+        if ".DS_Store" not in file:
+            with open(os.path.join(dataset_path, file), "r", encoding="utf8") as stream:
+                data += stream.read()
 
-    with open(os.path.join("../training/", training_file_path), "w", encoding="utf8") as training_file:
+    training_file_name = "{}.txt".format(os.path.basename(dataset_path))
+    training_file_path = os.path.join("../training/", training_file_name)
+
+    with open(training_file_path, "w", encoding="utf8") as training_file:
         training_file.write(data)
 
     return training_file_path
@@ -40,7 +51,14 @@ def main():
     trainer.generate_model("../training/Seuss.txt", "../models/Seuss.pkl")
     trainer.generate_model("../training/Shakespeare.txt", "../models/Shakespeare.pkl")
 
-    # Test Models
+    # Generate Testing Files
+    generate_testing_file("../training/Dickens.txt", "../testing/Dickens.txt")
+    generate_testing_file("../training/GRRM.txt", "../testing/GRRM.txt")
+    generate_testing_file("../training/JK Rowling.txt", "../testing/JK Rowling.txt")
+    generate_testing_file("../training/Mark Twain.txt", "../testing/Mark Twain.txt")
+    generate_testing_file("../training/Seuss.txt", "../testing/Seuss.txt")
+    generate_testing_file("../training/Shakespeare.txt", "../testing/Shakespeare.txt")
+
     testing_files = [
         "../testing/Dickens.txt",
         "../testing/GRRM.txt",
@@ -59,6 +77,7 @@ def main():
         "../models/Shakespeare.pkl"
     ]
 
+    # Test Models
     tester = ModelTester()
 
     print("\n")
